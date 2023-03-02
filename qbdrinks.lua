b709b5808f6c0a8d0b3572510361d383d3a65290
Lennya94 <53213636+Lennya94@users.noreply.github.com>
2023-03-02 19:50:38 +0000

    Add files via upload

---
+++ qbdrinks.lua
@@ -0,0 +1,261 @@
+['Barr-Cherryade'] = {
+	['name'] = 'Barr-Cherryade', 			
+	['label'] = 'Barr Cherryade', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Barr-Cherryade.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Barr-Barr-Cola'] = {
+	['name'] = 'Barr-Cola', 			
+	['label'] = 'Barr Cola', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Barr-Cola.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Barr-CreamSoda'] = {
+	['name'] = 'Barr-CreamSodae', 			
+	['label'] = 'Barr CreamSoda', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Barr-CreamSoda.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Barr-Lemonade'] = {
+	['name'] = 'Barr-Lemonade', 			
+	['label'] = 'Barr Lemonade', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Barr-Lemonade.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Fanta-Grape'] = {
+	['name'] = 'Fanta-Grape', 			
+	['label'] = 'Fanta Grape', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Fanta-Grape.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Fanta-Grape'] = {
+	['name'] = 'Fanta-Grape', 			
+	['label'] = 'Fanta-Grape', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Fanta-Grape.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Fanta-Grape'] = {
+	['name'] = 'Fanta-Grape', 			
+	['label'] = 'Fanta-Grape', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Fanta-Grape.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['KA-FruitPunch'] = {
+	['name'] = 'KA-FruitPunch', 			
+	['label'] = 'KA-FruitPunch', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'KA-FruitPunch.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['KA-Pineapple'] = {
+	['name'] = 'KA-Pineapple', 			
+	['label'] = 'KA-Pineapple', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'KA-Pineapple.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['KA-Strawberry'] = {
+	['name'] = 'KA-Strawberry', 			
+	['label'] = 'KA-Strawberry', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'KA-Strawberry.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['KA-TropicalKrush'] = {
+	['name'] = 'KA-TropicalKrush', 			
+	['label'] = 'KA-TropicalKrush', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'KA-TropicalKrush.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Oasis-BlackcurrentApple'] = {
+	['name'] = 'Oasis-BlackcurrentApple', 			
+	['label'] = 'Oasis-BlackcurrentApple', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Oasis-BlackcurrentApple.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+
+
+['Oasis-CitrusPunch'] = {
+	['name'] = 'Oasis-CitrusPunch', 			
+	['label'] = 'Oasis-CitrusPunch', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Oasis-CitrusPunch.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Oasis-SummerFruits'] = {
+	['name'] = 'Oasis-SummerFruits', 			
+	['label'] = 'Oasis-SummerFruits', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Oasis-SummerFruits.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Prime-Orange'] = {
+	['name'] = 'Prime-Orange', 			
+	['label'] = 'Prime-Orange', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Prime-Orange.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Prime-MetaMoon'] = {
+	['name'] = 'Prime-MetaMoon', 			
+	['label'] = 'Prime-MetaMoon', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Prime-MetaMoon.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Prime-LemonLime'] = {
+	['name'] = 'Prime-LemonLime', 			
+	['label'] = 'Prime-LemonLime', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Prime-LemonLime.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Prime-IcePop'] = {
+	['name'] = 'Prime-IcePop', 			
+	['label'] = 'Prime-IcePop', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Prime-IcePop.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Prime-Grape'] = {
+	['name'] = 'Prime-Grape', 			
+	['label'] = 'Prime-Grape', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Prime-Grape.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
+
+['Prime-BlueRaspberry'] = {
+	['name'] = 'Prime-BlueRaspberry', 			
+	['label'] = 'Prime-BlueRaspberry', 		
+	['weight'] = 100, 		
+	['type'] = 'item', 
+	['image'] = 'Prime-BlueRaspberry.png', 
+	['unique'] = false, 
+	['useable'] = true, 
+	['shouldClose'] = true, 
+	['combinable'] = nil, 
+	['description'] = 'Your description here'
+},
